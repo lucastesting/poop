d765520acdf81fa6a670c7a7bb9100dac53895c4
Lucas Parry <lparry@gmail.com>
2016-05-09 21:42:06 +1000

    poop

--- components/Html/Html.js
+++ components/Html/Html.js
@@ -19,7 +19,7 @@ function Html({ title, description, body, debug }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto" />
-        <script src={'/app.js?' + new Date().getTime()} />
+        <script src={'poop/app.js?' + new Date().getTime()} />
       </head>
       <body>
         <div id="app" dangerouslySetInnerHTML={{ __html: body }} />
